cfb14301cb95bea5e889dd9149179a482b287acf
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-04-20 00:05:09 +0530

    Add files via upload

---
+++ Python/16_Function.py
@@ -0,0 +1,21 @@
+# def new():
+#     print("Hello")
+#     print("Good Morning.")
+#     print("")
+# new()
+
+
+
+# def sum (a,b):
+#     solve = a + b
+#     return solve
+
+# print(sum(5,10))
+
+
+def avg(a = 2, b = 2 ,c = 2):
+    calc = (a + b + c)/ 3
+    return calc
+
+print(avg(2,4,6))
+print(avg())
