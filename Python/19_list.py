bafbbaa3bf299f2660f18dfd4319c788bee00c1b
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-04-20 00:06:07 +0530

    Add files via upload

---
+++ Python/19_list.py
@@ -0,0 +1,53 @@
+# list - mutable
+
+# marks = [12,45,67,34,34,35,6,99]
+# print(marks)
+# marks[6] = 56
+# marks[4] = 46
+
+# print(marks)
+
+# print(marks[1:])
+
+# list method
+
+l = [1,2,3]
+
+# l.append(4)
+# l.insert(4,5)
+# print(l)
+
+s = [2,5,8,9,6,3,1,4,7]
+# print(s)
+# s.sort()
+# print(s)
+# s.sort(reverse=True)
+# print(s)
+# s.reverse()
+# print(s)
+
+
+# for val in s:
+    # print(val)
+
+x = 1
+idx = 0
+for value in s:
+    if(value == x):
+        print(f"{x} Found at index: {idx}")
+        break
+    idx += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
