2b448753d661feb6b7fadea3d28085bfd46b6d94
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-05-03 13:06:57 +0530

    Add files via upload

---
+++ Python/PYTHON_PRACTICE/Practice1.py
@@ -0,0 +1,30 @@
+# CHAPTER 2 – PRACTICE SET
+# 1
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+print(a+b)
+
+# 2
+num = 19
+z = 7
+print(num % z)
+
+# 3
+value = input("Enter: ")
+print(type(value))
+
+# 4
+a = 80
+b = 43
+print(a>b)
+
+# 5
+a = 3
+b = 89
+c = 16
+x = (a+b+c)/3
+print(x)
+
+# 6
+x = int(input("Enter a value: "))
+print(x*x)
