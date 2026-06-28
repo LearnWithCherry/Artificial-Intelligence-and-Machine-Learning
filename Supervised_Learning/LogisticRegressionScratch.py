0c12a0af47aab22632add4489c1f8aea734c1764
Rajat Bhardwaj <rajatb1008@gmail.com>
2026-06-28 15:21:23 +0530

    Fix logistic regression implementation errors

--- Supervised_Learning/LogisticRegressionScratch.py
+++ Supervised_Learning/LogisticRegressionScratch.py
@@ -1,34 +1,47 @@
+# Logistic Regression
 import numpy as np
 class LogisticRegression:
-    def __init__(self, learning_rate = 0.1, n_iter = 1000):
+    def __init__(self, learning_rate=0.1, n_iter=1000):
         self.bias = None
-        self.weight = None
+        self.weights = None
         self.lr = learning_rate
         self.n_iter = n_iter
 
-    def _sigmoid(z):
-        return (1/(1*np.exp(-z)))
-    
+    def _sigmoid(self, z):
+        return (1 / (1 + np.exp(-z)))
+
     def fit(self, X, y):
         m, n = X.shape
 
         self.bias = 0
-        self.weight = np.zeros(n)
+        self.weights = np.zeros(n)
+
         for i in range(self.n_iter):
-            z = self.bias * np.dot(X.T, self.weight)
+            z = self.bias + np.dot(X, self.weights)
             y_pred = self._sigmoid(z)
 
-            db = (1/m)*np.sum(y_pred-y)
-            dw = (1/m)*np.sum*(X.T, (y_pred-y))
-
+            db = (1/m) * (y_pred - y)
+            dw = (1/m) * np.dot(X.T, (y_pred - y))
+            
             self.bias -= self.lr * db
-            self.weight -= self.lr * dw
-
+            self.weights -= self.lr * dw
+    
     def get_probabilities(self, X):
-        z = self.bias * np.dot(X.T, self.weight)
+        z = self.bias + np.dot(X, self.weights)
         return self._sigmoid(z)
+        
+    def predict(self, X, threshold=0.5):
+        probabilities = self.get_probabilities(X)
+        y_pred_bool = probabilities >= threshold
+        return y_pred_bool.astype(int)
+    
+X = np.array([[1, 2], [2, 3], [3, 4], [4, 5]])
+y = np.array([0, 0, 1, 1])
 
-    def predict(self, X):
-        prob = self.get_probabilities(X)
-        y_pred_bool = prob >= threshold
+model = LogisticRegression()
+model.fit(X, y)
 
+y_pred = model.predict(X)
+print(y_pred)
+
+print(model.get_probabilities(X))
